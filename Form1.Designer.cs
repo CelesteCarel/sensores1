4513624d0c0c56fe5e95324f24e7f37dbfaafbac
CelesteCarel <Celeste.corvcab787@gmail.com>
2024-10-31 14:44:59 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace sensores1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.limTem = new System.Windows.Forms.Label();
+            this.lbConection = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbTemp = new System.Windows.Forms.Label();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.btnDesconectar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // limTem
+            // 
+            this.limTem.AutoSize = true;
+            this.limTem.Location = new System.Drawing.Point(43, 53);
+            this.limTem.Name = "limTem";
+            this.limTem.Size = new System.Drawing.Size(112, 13);
+            this.limTem.TabIndex = 0;
+            this.limTem.Text = "Limite de Temperatura";
+            // 
+            // lbConection
+            // 
+            this.lbConection.AutoSize = true;
+            this.lbConection.Location = new System.Drawing.Point(43, 168);
+            this.lbConection.Name = "lbConection";
+            this.lbConection.Size = new System.Drawing.Size(69, 13);
+            this.lbConection.TabIndex = 1;
+            this.lbConection.Text = "Conexión OK";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(240, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Temperatura";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(46, 89);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // lbTemp
+            // 
+            this.lbTemp.AutoSize = true;
+            this.lbTemp.Location = new System.Drawing.Point(259, 140);
+            this.lbTemp.Name = "lbTemp";
+            this.lbTemp.Size = new System.Drawing.Size(28, 13);
+            this.lbTemp.TabIndex = 4;
+            this.lbTemp.Text = "00.0";
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(37, 221);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(75, 23);
+            this.btnConectar.TabIndex = 5;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // btnDesconectar
+            // 
+            this.btnDesconectar.Location = new System.Drawing.Point(214, 221);
+            this.btnDesconectar.Name = "btnDesconectar";
+            this.btnDesconectar.Size = new System.Drawing.Size(93, 23);
+            this.btnDesconectar.TabIndex = 6;
+            this.btnDesconectar.Text = "Desconectar";
+            this.btnDesconectar.UseVisualStyleBackColor = true;
+            this.btnDesconectar.Click += new System.EventHandler(this.btnApagar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 309);
+            this.Controls.Add(this.btnDesconectar);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.lbTemp);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbConection);
+            this.Controls.Add(this.limTem);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label limTem;
+        private System.Windows.Forms.Label lbConection;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbTemp;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Button btnDesconectar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Drawing.Configuration;
+
+namespace sensores1
+{
+    public partial class Form1 : Form
+    {
+        delegate void SetTextDelegate(string value);
+        public SerialPort ArduinoPort
+        {
+            get;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            ArduinoPort = new System.IO.Ports.SerialPort();
+            ArduinoPort.PortName = "COM6";
+            ArduinoPort.BaudRate = 9600;
+            ArduinoPort.DataBits = 8;
+            ArduinoPort.ReadTimeout = 1000;
+            ArduinoPort.WriteTimeout = 1000;
+            ArduinoPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            //ArduinoPort.Open();
+            this.btnConectar.Click += btnConectar_Click; 
+        }
+
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            string dato = ArduinoPort.ReadLine();
+            //lbTemp.Text = indata;
+            EscribirTxt(dato);
+        }
+
+        private void EscribirTxt(string dato)
+        {
+            if (InvokeRequired)
+                try
+                {
+                    Invoke(new SetTextDelegate(EscribirTxt), dato);
+
+                }
+                catch
+                {
+
+                }
+            else
+                lbTemp.Text = dato;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnApagar_Click(object sender, EventArgs e)
+        {
+            btnConectar.Enabled = true;
+            btnDesconectar.Enabled = false;
+            if (ArduinoPort.IsOpen)
+                ArduinoPort.Close();
+            lbConection.Text = "Desconectado";
+            lbConection.ForeColor = System.Drawing.Color.Red;
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            //btnDesconectar.Enabled = true;
+            //btnConectar.Enabled = false;
+            try
+            {
+                if (!ArduinoPort.IsOpen)
+                    ArduinoPort.Open();
+                if (int.TryParse(limTem.Text,out int temperatureLimit))
+                    {
+                    string limitString = temperatureLimit.ToString();
+                    ArduinoPort.Write(limitString);
+                }
+                else
+                {
+                    MessageBox.Show("Ingresa un valor númerico válido");
+                }
+                lbConection.Text = "Conexión Ok";
+                lbConection.ForeColor = System.Drawing.Color.Lime;
+            }
+            catch
+            {
+                MessageBox.Show("Configure el puerto de comunicación correcto");
+            }
+
+
+        }
+    }
+}
